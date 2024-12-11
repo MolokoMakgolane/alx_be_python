62164b307cd8f25e18bf42c301cddcc30dacd818
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2024-12-11 15:18:59 +0200

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-simple interest = principal * rate * time
+interest = principal * rate * time
 
-print(f"simple interest = principal * rate * time")
+print(f"interest = principal * rate * time")
