cccf9f271c0f802a6fc8c3159919e85830e97280
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2024-12-19 15:22:28 +0200

    Create daily_reminder.py

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,24 @@
+task = input("Enter your task:")
+priority = input("Priority (high/medium/low):")
+time_bound = input("Is it time-bound? (yes/no):")
+error_message = "something went wrong!"
+first_part = f"{task} is a {priority} priority task"
+match priority:
+    case "high":
+        reminder = "that requires immediate attention today!"
+    case "medium":
+       reminder = "schedule it for later"
+    case "low":
+        reminder = "Consider completing it when you have free time."
+    case _ :
+        message = error_message
+     if time_bound == "yes":
+    reminder = "that requires immediate attention today!"
+elif time_bound == "no":
+    reminder = "Consider completing it when you have free time."
+else:
+    message = error_message 
+message = (f"Reminder: '{task}' is a {priority} priority task. that requires immediate attention today!" 
+           if time_bound == "yes" else "Consider completing it when you have free time.")
+print("Reminder: ", reminder)
+print(message)
