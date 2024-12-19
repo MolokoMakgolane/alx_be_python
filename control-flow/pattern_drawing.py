d85dcdb82263472afa2816368ee5115c63ec4c0b
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2024-12-19 15:18:52 +0200

    Create  pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+size = int(input('ENTER THE SIZE OF THE PATTERN:'))
+tracker = 0
+while tracker < size:
+
+    for i in range(size):
+        print('*', end='')
+    print()
+    tracker += 1
+for i in range(size):
+        print('*', end='')
+    print() 
+    tracker += 1
