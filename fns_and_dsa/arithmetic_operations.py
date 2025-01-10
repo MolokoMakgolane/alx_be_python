b53a73d4aee77a05909c9138e2db9fdcd04e5dd3
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2025-01-10 15:02:11 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
-    if operation = 'add':
+    if operation == 'add':
        return num1 + num2
-    elif operation = 'subtract':
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation  = 'multiply':
+    elif operation == 'multiply':
         return num1 * num2
-    elif operation = 'divide':
-         if num2 = 0:
+    elif operation == 'divide':
+         if num2 == 0:
            'return(cannot divide by 0')
          else:
            return num1/num2
